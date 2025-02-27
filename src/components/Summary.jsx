0c4750ddbb85d9f426c2e915812d8924e0e68a1b
Tanya Dubey <145438809+tanyadubey9@users.noreply.github.com>
2025-02-27 14:52:24 +0530

    added Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -18,7 +18,7 @@ function Summary() {
           </div>
           <button type="button">
           <a 
-            href="/Images/resume.pdf" 
+            href="/Tanya-portfolio/resume.pdf" 
             download="Tanya_Dubey_Resume.pdf" 
             className="flex gap-1 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-5"
           > Resume <img src={download} alt="download" width={20} /> </a>
